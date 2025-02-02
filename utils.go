7580d06b3ff547a1fbc76493336a89c6a19b260a
Josh Montoya <josh@montoya.io>
2025-02-02 12:48:55 -0800

    implement getKeyBytes

--- utils.go
+++ utils.go
@@ -14,3 +14,13 @@ func makeIVFromCrypto() (iv []byte, err error) {
 	_, err = io.ReadFull(rand.Reader, iv)
 	return
 }
+
+// Function to ensure the key is 16 bytes long for AES-128
+func getKeyBytes(key string) []byte {
+	keyBytes := []byte(key)
+	if len(keyBytes) != 16 {
+		// You can truncate or pad the key if necessary, but AES-128 requires 16 bytes
+		keyBytes = append(keyBytes, make([]byte, 16-len(keyBytes))...)
+	}
+	return keyBytes
+}
