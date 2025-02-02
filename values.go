0af02a608de28d8d02db1defb81824f56deeeec9
Josh Montoya <josh@montoya.io>
2025-02-02 12:49:35 -0800

    implement values

---
+++ values.go
@@ -0,0 +1,24 @@
+package secrets
+
+import "encoding/json"
+
+func makeValues(str, key string) (out Values, err error) {
+	if len(str) == 0 {
+		return
+	}
+
+	var decrypted []byte
+	if decrypted, err = decrypt(str, key); err != nil {
+		return
+	}
+
+	err = json.Unmarshal(decrypted, &out)
+	return
+}
+
+type Values map[string]string
+
+func (v Values) String() string {
+	bs, _ := json.Marshal(v)
+	return string(bs)
+}
