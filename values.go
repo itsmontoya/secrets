b66dcd83bbb5dc7ace9eb2768eb832e286d06019
Josh Montoya <josh@montoya.io>
2025-02-03 21:47:11 -0800

    remove makeValuesFromString

--- values.go
+++ values.go
@@ -6,20 +6,6 @@ func makeValues() (out Values) {
 	return make(Values)
 }
 
-func makeValuesFromString(str, key string) (out Values, err error) {
-	if len(str) == 0 {
-		return
-	}
-
-	var decrypted []byte
-	if decrypted, err = decrypt(str, key); err != nil {
-		return
-	}
-
-	err = json.Unmarshal(decrypted, &out)
-	return
-}
-
 type Values map[string]string
 
 func (v Values) String() string {
